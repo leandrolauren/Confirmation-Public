ddc488739a90bf50a145fa18bce1cd601f240592
Leandro Laurenzette <leandrolaurenzette@gmail.com>
2025-02-27 23:37:15 -0300

    mensagem corrigida com .gitignore e arquivos removidos

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import ConfirmationForm from './components/ConfirmationForm';
+import MapComponent from './components/MapComponent';
+import Information from './components/Information';
+
+function App() {
+
+  return (
+    <Container maxWidth="sm">
+      <Information/>
+      <ConfirmationForm />
+      <MapComponent />
+
+    </Container>
+  );
+}
+
+export default App;
